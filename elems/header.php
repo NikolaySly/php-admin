90fb43ef71d9e26b01f82075e190bc7fb306aa0b
NikolaySly <slyadnev91@mail.ru>
2021-06-03 23:58:20 +0600

    func

--- elems/header.php
+++ elems/header.php
@@ -1,3 +1,16 @@
 <a <?php if ($_GET['page'] == 'index' or $_GET['page'] == '') {echo 'class ="active " ';}?>  href="/">главная</a>
 <a <?php if ($_GET['page'] == 'contacts') {echo 'class ="active " ';}?> href="?page=contacts">контакты</a>
 <a <?php if ($_GET['page'] == 'about') {echo 'class ="active " ';}?>  href="?page=about">о нас</a>
+
+<?php
+/*
+function creatLink($href, $text)
+{
+    
+}
+
+*/
+
+
+
+?>
